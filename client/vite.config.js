2349dcc1dbf3faa35bdc837d198b1f9b54cbce03
Aarush-394 <arg.39452161@gmail.com>
2025-05-14 19:56:41 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';  // Import react plugin from @vitejs/p
  // Ensure tailwindcss is imported
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],  // Use the react plugin
+  plugins: [react()],  // Use the react plugin
   server: {
     proxy: {
       '/api': 'https://auth-mern-backend-9pgx.onrender.com',
