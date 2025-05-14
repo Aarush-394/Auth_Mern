8838bd65de05a89e53c7b92f2e1d540ba51671d3
Aarush-394 <arg.39452161@gmail.com>
2025-05-14 19:51:03 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,10 @@
+import { defineConfig } from 'vite';  // Add this import
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      '/api': 'https://auth-mern-backend-9pgx.onrender.com'
+      '/api': 'https://auth-mern-backend-9pgx.onrender.com',
     },
   },
 });
