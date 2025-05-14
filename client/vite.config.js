9aa0b64dae21717a0bf90b888bb3b231bafa517f
Aarush-394 <arg.39452161@gmail.com>
2025-05-14 19:34:20 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      '/api': {
-        target: 'https://auth-mern-backend-9pgx.onrender.com',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
-})
+      '/api': 'https://auth-mern-backend-9pgx.onrender.com',  // Adjust the backend URL accordingly
+    },
+  },
+});
