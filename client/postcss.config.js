e07c009f7f906dfe424933e759d2f96558371d9d
Aarush-394 <arg.39452161@gmail.com>
2025-05-14 20:10:59 +0530

    mv postcss.config.js postcss.config.cjs

--- client/postcss.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},  // Add Tailwind CSS as a PostCSS plugin
-    autoprefixer: {}, // Optionally add Autoprefixer for cross-browser support
-  },
-};
