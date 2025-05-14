3018b575d8937af2d8cc8a56e5b66f6dc30e10ac
Aarush-394 <arg.39452161@gmail.com>
2025-05-14 12:27:24 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,5 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  
+  server: {
+    host: '0.0.0.0',                 // ← important for Render
+    port: import.meta.env.VITE_BACKEND_URL || 5173,  // ← use PORT from environment
+  }
 })
