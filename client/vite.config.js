e9669941cb09848a208df2bae6f47112c8ac891c
Aarush-394 <arg.39452161@gmail.com>
2025-05-15 12:02:27 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// Vite will auto-detect the PostCSS config, no need to explicitly reference it here
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
-    proxy: {
-      '/api': 'https://auth-mern-backend-9pgx.onrender.com',  // Adjust the backend URL accordingly
-    },
-  },
-});
+    host: '0.0.0.0',                 // ← important for Render
+    port: import.meta.env.VITE_BACKEND_URL || 5173,  // ← use PORT from environment
+  }
+})
